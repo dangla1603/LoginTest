21952e510ded53d91c100973ef0c351946d6b0d9
vdq150030 <43397463+vdq150030@users.noreply.github.com>
2020-02-18 12:12:08 -0600

    Update Camera.java

--- app/src/main/java/com/example/logintest/Camera.java
+++ app/src/main/java/com/example/logintest/Camera.java
@@ -32,6 +32,7 @@ public class Camera extends AppCompatActivity {
         final VideoView videoView = findViewById(R.id.videoview);
         listView =  findViewById(R.id.lvideo);
         videoList = new ArrayList<>();                  // Make ArrayList for video
+        // TODO: tcp send_command -> string -> list of available files -> for loop videoList.add() 
         videoList.add("video");
         videoList.add("video1");
         videoList.add("video2");
@@ -43,14 +44,23 @@ public class Camera extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //Toast.makeText(getApplicationContext(),position+"",Toast.LENGTH_SHORT).show();
+                
+                // NOTE: if possible try to get the URI path based on the name of the clicked element instead of index 
                 switch (position){                                                                              // Make the list of VideoView
-
+ 
                     case 0:
                         videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName()+ "/" +R.raw.example));
                         break;
                     case 1:
                         videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName()+"/" +R.raw.video));
                         break;
+                    /* NOTE: 
+                    case "video-2020_2_18.mp4":
+                            tcp send_command("/GET video-2020_2_18.mp4") 
+                            -> file write to sd card with the same name 
+                            *wait for file to download*
+                            -> videoView.setVideoURI(URI.parse("the path we just write to"))
+                    */
                 }
                 videoView.setMediaController(new MediaController(Camera.this));             //add MediaController for the video
                 videoView.requestFocus();
