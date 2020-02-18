730a5b9612022ba55230c68f5642e92bbcbb3960
dangla1603 <43182305+dangla1603@users.noreply.github.com>
2020-02-18 11:32:38 -0600

    Add ListView() into Camera class
    Add Listview() function into VideoView() so we can make a list of Video

--- app/src/main/java/com/example/logintest/Camera.java
+++ app/src/main/java/com/example/logintest/Camera.java
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import java.util.ArrayList;
 
 public class Camera extends AppCompatActivity {
 
+    ListView listView;
+    ArrayList<String> videoList;
+    ArrayAdapter adapter;
 
 
     @Override
@@ -20,15 +26,45 @@ public class Camera extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
-        VideoView videoView = findViewById(R.id.videoview);
+        final VideoView videoView = findViewById(R.id.videoview);
+        listView =  findViewById(R.id.lvideo);
+        videoList = new ArrayList<>();                  // Make ArrayList for video
+        videoList.add("video");
+        videoList.add("video1");
+        videoList.add("video2");
+        videoList.add("video3");
+
+        adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,videoList);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Toast.makeText(getApplicationContext(),position+"",Toast.LENGTH_SHORT).show();
+                switch (position){                                                                              // Make the list of VideoView
+
+                    case 0:
+                        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName()+ "/" +R.raw.example));
+                        break;
+                    case 1:
+                        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName()+"/" +R.raw.video));
+                        break;
+                }
+                videoView.setMediaController(new MediaController(Camera.this));             //add MediaController for the video
+                videoView.requestFocus();
+                videoView.start();
+
+            }
+        });
 
 
-        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.example;
+
+
+       /* String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.example;
         Uri uri = Uri.parse(videoPath);
         videoView.setVideoURI(uri);
 
         MediaController mediaController = new MediaController(this);
         videoView.setMediaController(mediaController);
-        mediaController.setAnchorView(videoView);
+        mediaController.setAnchorView(videoView); */
     }
 }
