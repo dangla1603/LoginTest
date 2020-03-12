c03e522f417c710beebdcef4511f093ec2f1b114
dangla1603 <43182305+dangla1603@users.noreply.github.com>
2020-03-12 09:42:58 -0500

    change the icon on second activity
    add Reset button into second activiy, need to do some test to see how that work

--- app/src/main/java/com/example/logintest/SecondActivity.java
+++ app/src/main/java/com/example/logintest/SecondActivity.java
@@ -2,11 +2,15 @@ package com.example.logintest;
 
         import androidx.appcompat.app.AppCompatActivity;
 
+        import android.app.Activity;
+        import android.app.ProgressDialog;
         import android.content.Intent;
+        import android.os.AsyncTask;
         import android.os.Bundle;
         import android.view.View;
         import android.widget.Button;
         import android.widget.ImageView;
+        import android.widget.TextView;
 
         import com.google.firebase.auth.FirebaseAuth;
 
@@ -17,6 +21,8 @@ public class SecondActivity extends AppCompatActivity {
     private ImageView Camera;
     private ImageView Temperature;
     private ImageView Status;
+    private Button Reset;
+    Activity activity_ctx;
 
 
     @Override
@@ -64,5 +70,38 @@ public class SecondActivity extends AppCompatActivity {
                 startActivity(new Intent(SecondActivity.this,Temperature.class));
             }
         });
+
+        Reset = (Button) findViewById(R.id.btnReset);           // this function will reset the device from the app
+
+        Reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String QUERY_TEMPERATURE_DATA = "reset";                    // send signal " command reset " to pi
+                new server_connection().execute(QUERY_TEMPERATURE_DATA);
+            }
+        });
+
+    }
+    // using asyncTask to function reset button
+    private class server_connection extends AsyncTask<Object, Void, Object> {
+
+        ProgressDialog progress = new ProgressDialog(activity_ctx);
+
+        @Override
+        protected Object doInBackground(Object... voids){
+            TCPClient pi = new TCPClient("192.168.1.3", 8000, activity_ctx);
+            return pi.send_command((String)voids[0]);
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+            // Display loading annimation
+        }
+
+        @Override
+        protected void onPostExecute(Object result) {
+
+
+        }
     }
 }
+
